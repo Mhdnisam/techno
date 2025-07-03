410ec8b931bc690304b7f021e66f60b3503df3dc
Mhdnisam <mhdnisamm@gmail.com>
2025-07-03 15:58:57 +0530

    3/07

--- task/freepage.py
+++ task/freepage.py
@@ -1,1 +1,105 @@
-# just test
+# arithemetic operator
+
+# a = int(input("enter a number"))
+# b = int(input("enter a number"))
+# print("addition =", a + b)
+# print("subtraction =", a - b)
+# print("division =", a / b)
+# print("mul = ", a * b)
+# print("modulus =", a % b)
+# print("floor division", a // b)
+# print("exponentiation = ", a ** b)
+
+# comparison operator
+# if both values are same
+
+# a = int(input("enter a number"))
+# b = int(input("enter a number"))
+# if a == b:
+#     print("true")
+# else:
+#     print("false")
+
+# if one number is greater
+# a = int(input("enter a number"))
+# b = int(input("enter a number"))
+# if a > b:
+#     print("a is greater")
+# elif b > a:
+#     print("b is greater")
+# else:
+#     print("both number a equal")
+
+# if  a number is smaller
+
+# a = int(input("enter a number"))
+# b = int(input("enter a number"))
+# if a < b:
+#     print("a is smaller")
+# elif b < a:
+#     print("b is smaller")
+# else:
+#     print("both are equal")
+
+# if two number are not  equal
+#
+# a = int(input("enter a number"))
+# b = int(input("enter a number"))
+# if a == b:
+#     print("both are rqual")
+# else:
+#     print("not equal")
+
+# # person passed
+# a = int(input("enter a number"))
+# if a <= 33:
+#     print("true")
+# else:
+#     print("false")
+
+# # logical operator
+
+# and operator(Check if a number is greater)
+
+# a = 10
+# b = 5
+# if b >= 5 and a <= 10:
+#     print("true")
+# else:
+#     print("false")
+#
+
+# or operator(Check if a number is less than 3 or greater than 10)
+# a = 12
+# b = 5
+# if a <= 3 or a >= 10:
+#     print("true")
+# else:
+#     print("false")
+#
+
+# # not operator(Operator  Set x = True and print not x.)
+# a = 10
+# b = 45
+# print(not a > b)
+#
+# x = True
+# print("x =", not x)
+
+
+# combine conditions(Write a program to check if a person is above 18 and has an ID card.)
+# name = input("enter your name")
+# age = int(input("enter your age"))
+# place = input("enter your place")
+#     if age <= 18:
+#         print("doesn't create you id card")
+#
+
+# username = input("enter your username")
+# password = int(input("enter yout password"))
+# if password == 1234:
+#     print("access granded")
+# else:
+#     print("access denied")
+#
+
